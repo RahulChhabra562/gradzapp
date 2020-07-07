d8dfd88509f5c7d747087e2812f1375798783c5d
Rahul Chhabra <chhabra.rahul562@gmail.com>
2020-07-08 02:32:36 +0530

    Userapplication router completed with proper User authentication

--- routes/userapplicationrouter.js
+++ routes/userapplicationrouter.js
@@ -3,6 +3,7 @@ const authenticate = require('../authenticate');
 const Userapplications = require('../models/userapplication');
 
 const bodyParser = require('body-parser');
+const user = require('../models/user');
 
 
 
@@ -51,6 +52,95 @@ userapplicationRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
     
+})
+
+userapplicationRouter.route('/:userapplicationId')
+
+.get(authenticate.verifyUser, (req,res,next) => {
+    Userapplications.findById(req.params.userapplicationId)
+    .then((userapplication) => {
+        if(userapplication) {
+            if (req.user._id.equals(userapplication.user)) {
+                res.statusCode = 200;
+                res.setHeader('Content-Type','application/json');
+                res.json(userapplication);
+            }
+    
+            else {
+                res.statusCode = 401;
+                res.end('You can have access to your applications only');
+            }
+        }
+        else {
+            const err = new Error('User application not found.')
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, err => next(err))
+    .catch((err) => next(err));
+})
+
+.post(authenticate.verifyUser, (req,res,next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /userapplications/' + req.params.userapplicationId);
+})
+
+.put(authenticate.verifyUser, (req,res,next) => {
+    Userapplications.findById(req.params.userapplicationId)
+    .then((userapplication) => {
+        if(userapplication) {
+            if(req.user._id.equals(userapplication.user)) {
+                Userapplications.findByIdAndUpdate(req.params.userapplicationId, {
+                    $set: req.body
+                }, {new: true})
+                .then((userapplication) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type','application/json');
+                    res.json(userapplication)
+                })
+                
+            }
+    
+            else {
+                res.statusCode = 401;
+                res.end('You can edit only your applications')
+            }
+        }
+        else {
+            const err = new Error('User application not found.')
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, err => next(err))
+    .catch((err) => next(err));
+})
+
+.delete(authenticate.verifyUser, (req,res,next) => {
+    Userapplications.findById(req.params.userapplicationId)
+    .then((userapplication) => {
+        if(userapplication) {
+            if(req.user._id.equals(userapplication.user)) {
+                Userapplications.findByIdAndRemove(req.params.userapplicationId)
+                .then((resp) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(resp);
+                }, (err) => next(err))
+                .catch((err) => next(err));
+                
+            } 
+            else {
+                res.statusCode = 401;
+                res.end('You can delete only your applications')
+            }
+        }
+        else {
+            const err = new Error('User application not found.')
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, err => next(err))
+    .catch((err) => next(err))
 })
 
 
