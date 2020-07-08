9ca7967e4ad80f549d4d7274af6ff5d52843bd3b
Rahul Chhabra <chhabra.rahul562@gmail.com>
2020-07-09 03:31:45 +0530

    GET USERS GET particular user appliations done

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,24 @@ const bodyParser = require('body-parser');
 const User = require('../models/user')
 const passport = require('passport')
 const authenticate = require('../authenticate');
+const Userapplications = require('../models/userapplication');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+    User.find({})
+    .then((users) => {
+        if(!users) {
+            const err = new Error('No users are registered.');
+            err.statusCode = 404;
+            return next(err);
+        }
+        else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(users);
+        }
+    },err => next(err))
+    .catch((err) => next(err));
 });
 
 router.post('/signup', (req, res, next) => {
@@ -46,5 +60,55 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
+
+router.get('/:userId', authenticate.verifyUser, (req,res,next) => {
+    User.findById(req.params.userId)
+    .then((user) => {
+        if(!user) {
+            const err = new Error('User with id ' + req.params.userId + 'not found.')
+            err.statusCode = 404;
+            return next(err);
+        }
+
+        else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(user);
+        }
+    }, err => next(err))
+    .catch((err) => next(err))
+})
+
+router.get('/:userId/applications', authenticate.verifyUser, (req,res,next) => {
+    User.findById(req.params.userId)
+    .then((user) => {
+        if(!user) {
+            const err = new Error('User not found with id' + req.params.userId);
+            err.statusCode = 404;
+            return next(err);
+        }
+
+        else {
+            Userapplications.find({user: req.params.userId})
+            .populate('institution')
+            .populate('course')
+            .then((userapplications) => {
+                if(!userapplications) {
+                    const err = new Error('User has not applied in any courses.');
+                    err.statusCode = 400;
+                    return next(err);
+                }
+
+                else {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type','application/json');
+                    res.json(userapplications);
+                }
+            }, err => next(err))
+            .catch((err) => next(err));
+        }
+    }, err => next(err))
+    .catch((err) => next(err));
+})
 
 module.exports = router;
